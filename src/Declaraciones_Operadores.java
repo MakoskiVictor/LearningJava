af6efb394f992f9eb311090550b0d035fe326679
MakoskiVictor <makoski.ed@gmail.com>
2023-11-10 16:19:17 -0300

    Practice with constants and operatos

---
+++ src/Declaraciones_Operadores.java
@@ -0,0 +1,30 @@
+
+public class Declaraciones_Operadores {
+
+	public static void main(String[] args) {
+
+//		double a = 5;
+//
+//		double b;
+//
+//		b = 7;
+//
+//		double c = b / a;
+
+//		c++;
+//
+//		c+=6;
+//
+//		System.out.println(c);
+		
+		final double aPulgadas = 2.54;
+		
+		double cm = 6;
+		
+		double resultado = cm / aPulgadas;
+		
+		System.out.println("En " + cm + " cm hay " + resultado + " pulgadas");
+
+	}
+
+}
