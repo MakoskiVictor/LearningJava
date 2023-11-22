fc2a35726f4750c89146ddc0fb7d811b2d004763
MakoskiVictor <makoski.ed@gmail.com>
2023-11-21 21:15:56 -0300

    desarrollador class

---
+++ src/poo_herencia/Desarrollador.java
@@ -0,0 +1,17 @@
+package poo_herencia;
+
+public class Desarrollador extends Empleado {
+
+	private String lenguaje;
+	
+	public Desarrollador (String nombre, int edad, String lenguaje) {
+		super(nombre,edad);
+		
+		this.lenguaje = lenguaje;
+	}
+	
+	public void programar () {
+		System.out.println("Programando en " + lenguaje);
+	}
+
+}
