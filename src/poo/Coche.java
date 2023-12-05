c3e6fdd11d44bb8d0e9f2ef6efd5db1eac12e740
MakoskiVictor <makoski.ed@gmail.com>
2023-12-05 20:58:29 -0300

    poo coche

---
+++ src/poo/Coche.java
@@ -0,0 +1,26 @@
+package poo;
+
+public class Coche {
+	
+	int ruedas;
+	int largo;
+	int ancho;
+	int motor;
+	int peso;
+	
+	public Coche () {
+		//ruedas, largo, ancho, motor, peso
+		//this.ruedas = ruedas;
+		//this.largo = largo;
+		//this.ancho = ancho;
+		//this.motor = motor;
+		//this.peso = peso;
+		
+		this.ruedas = 4;
+		this.largo = 3;
+		this.ancho = 2;
+		this.motor = 3000;
+		this.peso = 2500;
+	}
+
+}
