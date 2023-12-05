45b902a306e9886cf08a59bd107568ba7b42dad7
MakoskiVictor <makoski.ed@gmail.com>
2023-12-05 20:58:39 -0300

    poo uso-coche

---
+++ src/poo/Uso_Coche.java
@@ -0,0 +1,17 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Instanciar una clase
+		Coche Renault = new Coche();
+		
+		System.out.println(Renault.motor);
+		System.out.println(Renault.ruedas);
+		System.out.println(Renault.peso);
+
+	}
+
+}
